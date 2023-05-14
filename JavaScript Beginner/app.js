08cd352074ac37330668155ebd6d7ba9fd068200
wilfredoreyes1989 <122574839+wilfredoreyes1989@users.noreply.github.com>
2023-05-13 21:07:40 -0400

    Add files via upload

---
+++ JavaScript Beginner/app.js
@@ -0,0 +1,16 @@
+
+
+// Question 1. Return the Sum of Two Numbers
+// Create a function that takes two arguments as functions and return their sum.
+// Example -
+// addition(3,2) -> 5
+// addition(-3,-6) -> -9
+// addition(7,3) -> 10
+
+function addition(num1, num2) {
+    return num1 + num2
+}
+
+console.log(addition(3,2));
+console.log(addition(-3,-6));
+console.log(addition(7,3));
